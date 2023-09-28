14b72826823a520ebfa44e8c5b589bd07c723c29
Rafael Barrientos García <rbarrientosg@redcibercom.com.mx>
2023-09-28 13:50:59 -0600

    Add users in each project

---
+++ hijo2/app.js
@@ -0,0 +1,7 @@
+const persona = {
+  Nombre: "Juan",
+  Edad: 20,
+  Direccion: "Calle 1",
+};
+
+console.log("Nombre:", persona.Nombre, "\nEdad:", persona.Edad);
---
+++ hijo3/app.js
@@ -0,0 +1,7 @@
+const persona = {
+  Nombre: "Chacnhito",
+  Edad: 19,
+  Direccion: "Calle 45",
+};
+
+console.log("Nombre:", persona.Nombre, "\nEdad:", persona.Edad);
